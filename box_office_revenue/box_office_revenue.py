f144d99f74971571abde06ee9a185e865bbf35d5
Sawyer Hannel <sawyerhannel94@gmail.com>
2022-04-16 12:59:23 -0700

    Added Algorithms

--- box_office_revenue/box_office_revenue.py
+++ box_office_revenue/box_office_revenue.py
@@ -1,7 +1,38 @@
-import csv
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import mean_squared_error, max_error
+import math
+from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import Perceptron
 
-train_data = pd.read_csv('data/X_data.csv')
-test_data = pd.read_csv('data/y_data.csv')
+X = pd.read_csv('data/X_data.csv')
+y = pd.read_csv('data/y_data.csv')
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Defining the Random Forest Model 
+rf_model = RandomForestRegressor(random_state=1)
+
+# Fitting the model 
+rf_model.fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+# Calculating feature importance
+feat_importances = pd.Series(rf_model.feature_importances_, index=X_train.columns)
+feat_importances.nlargest().plot(kind='barh')
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+# X, y = load_digits(return_X_y=True)
+clf = Perceptron(tol=1e-3, random_state=0)
+clf.fit(X, y)
+clf.score(X, y)
+
+regr = MLPRegressor(random_state=1, max_iter=50000).fit(X_train, y_train)
+regr.score(X_test, y_test)
--- box_office_revenue/clean_data.py
+++ box_office_revenue/clean_data.py
@@ -1,6 +1,7 @@
 import json
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 
 metadata = pd.read_csv('box_office_revenue/data/movies_metadata.csv')
@@ -8,6 +9,9 @@ data = pd.read_csv('box_office_revenue/data/boxoffice.csv')
 
 metadata = metadata.loc[:, metadata.columns.intersection(['genres', 'title', 'runtime'])]
 df = metadata.merge(data, on='title')
+df = df.replace([np.inf, -np.inf], np.nan)
+df = df.dropna()
+df = df.reset_index()
 
 y = df['lifetime_gross']
 X = df.drop('lifetime_gross', axis=1)
@@ -22,6 +26,12 @@ for i in df['genres']:
 
 X['genre'] = genres_arr
 X = X.drop(['genres','rank'], axis=1)
+
+le = LabelEncoder()
+X['title'] = le.fit_transform(X['title'])
+X['genre'] = le.fit_transform(X['genre'].astype(str))
+X['studio'] = le.fit_transform(X['studio'])
+X['year'] = le.fit_transform(X['year'])
 
 X.to_csv('box_office_revenue/data/X_data.csv')
 y.to_csv('box_office_revenue/data/y_data.csv')
