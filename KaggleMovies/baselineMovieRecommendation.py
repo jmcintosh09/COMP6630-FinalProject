f61a751cac8f1f7da08eb730919a8452afad565a
Jasmine D. McIntosh <Jasmine.Mcintosh@develop.ds.amrdec.army.mil>
2022-04-29 16:20:36 -0500

    Updated movie recommendation baseline algorithm

--- KaggleMovies/baselineMovieRecommendation.py
+++ KaggleMovies/baselineMovieRecommendation.py
@@ -1,38 +1,10 @@
-from collections import Counter
-import math
-
-def knn(data, query, k, distance_fn, choice_fn):
-    neighbor_distances_and_indices = []
-    
-    # 3. For each example in the data
-    for index, example in enumerate(data):
-        # 3.1 Calculate the distance between the query example and the current
-        # example from the data.
-        distance = distance_fn(example[:-1], query)
-        
-        # 3.2 Add the distance and the index of the example to an ordered collection
-        neighbor_distances_and_indices.append((distance, index))
-    
-    # 4. Sort the ordered collection of distances and indices from
-    # smallest to largest (in ascending order) by the distances
-    sorted_neighbor_distances_and_indices = sorted(neighbor_distances_and_indices)
-    
-    # 5. Pick the first K entries from the sorted collection
-    k_nearest_distances_and_indices = sorted_neighbor_distances_and_indices[:k]
-    
-    # 6. Get the labels of the selected K entries
-    k_nearest_labels = [data[i][-1] for distance, i in k_nearest_distances_and_indices]
-
-    # 7. If regression (choice_fn = mean), return the average of the K labels
-    # 8. If classification (choice_fn = mode), return the mode of the K labels
-    return k_nearest_distances_and_indices , choice_fn(k_nearest_labels)
-
 def baseline(data, query, k, distance_fn, choice_fn):
     movie_matches = []
     for index, example in enumerate(data):
-        count = distance_fn(example[:-1], query)
+        match = distance_fn(example[:-1], query)
         
-        movie_matches.append((count, index))
+        if match:
+            movie_matches.append((match, index))
         
     sorted_movie_matches = sorted(movie_matches, reverse=True)
     
@@ -42,25 +14,11 @@ def baseline(data, query, k, distance_fn, choice_fn):
     
     return n_number_movies, choice_fn(k_nearest_labels)
 
-
 def matches(point1, point2):
-    count = 0
-    for i in range(len(point1)):
-        if (point1[i] > 0.0 and point2[i] > 0.0 and point1[i] == point2[i]):
-            count +=1
-    return count
-
-def mean(labels):
-    return sum(labels) / len(labels)
-
-def mode(labels):
-    return Counter(labels).most_common(1)[0][0]
-
-def euclidean_distance(point1, point2):
-    sum_squared_distance = 0
-    for i in range(len(point1)):
-        sum_squared_distance += math.pow(point1[i] - point2[i], 2)
-    return math.sqrt(sum_squared_distance)
+    for i in range(len(point1)):        
+        if (point1[i] != point2[i]):
+            return False        
+    return True
 
 def recommend_movies(data_file, movie_query, algorithm, k_recommendations):
     raw_movies_data = []
@@ -96,12 +54,54 @@ def recommend_movies(data_file, movie_query, algorithm, k_recommendations):
 
 if __name__ == '__main__':
     file = "KaggleMovies/data/cleanMovies.csv"
-    encanto = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0] # feature vector for Encanto
-    movie_title = "Encanto"
-    number_of_movies = 10
-    recommended_movies = recommend_movies(data_file=file, movie_query=encanto, algorithm=baseline, k_recommendations=number_of_movies)
+    number_of_movies = 15
+    
+    movie = [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # feature vector for movie
+    movie_title = "The Lost City"
+    recommended_movies = recommend_movies(data_file=file, movie_query=movie, algorithm=baseline, k_recommendations=number_of_movies)
 
     # Print recommended movie titles
     print ("Movie Recommendations for " + movie_title + ":")
+    print ("Number of Genres: 2")
     for recommendation in recommended_movies:
         print(recommendation[1])
+    print()
+    
+    #---------------------------------------------------------------------------------------------------------------
+        
+    movie = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # feature vector for movie
+    movie_title = "Turning Red"
+    recommended_movies = recommend_movies(data_file=file, movie_query=movie, algorithm=baseline, k_recommendations=number_of_movies)
+
+    # Print recommended movie titles
+    print ("Movie Recommendations for " + movie_title + ":")
+    print ("Number of Genres: 3")
+    for recommendation in recommended_movies:
+        print(recommendation[1])
+    print()
+    
+    #---------------------------------------------------------------------------------------------------------------
+    
+    movie = [1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0] # feature vector for movie
+    movie_title = "Morbius"
+    recommended_movies = recommend_movies(data_file=file, movie_query=movie, algorithm=baseline, k_recommendations=number_of_movies)
+
+    # Print recommended movie titles
+    print ("Movie Recommendations for " + movie_title + ":")
+    print ("Number of Genres: 4")
+    for recommendation in recommended_movies:
+        print(recommendation[1])
+    print()
+    
+    #---------------------------------------------------------------------------------------------------------------
+    
+    movie = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0] # feature vector for movie
+    movie_title = "Encanto"
+    recommended_movies = recommend_movies(data_file=file, movie_query=movie, algorithm=baseline, k_recommendations=number_of_movies)
+
+    # Print recommended movie titles
+    print ("Movie Recommendations for " + movie_title + ":")
+    print ("Number of Genres: 5")
+    for recommendation in recommended_movies:
+        print(recommendation[1])
+    print()
